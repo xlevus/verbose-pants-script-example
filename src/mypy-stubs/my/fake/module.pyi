b92432a920032feb90563d2100b09829b224b1a3
Chris Targett <chris@xlevus.net>
2021-11-18 12:05:19 +1300

    added code

---
+++ src/mypy-stubs/my/fake/module.pyi
@@ -0,0 +1,5 @@
+from typing import Tuple
+
+
+def a_tuple() -> Tuple[int, str]:
+    ...
---
+++ src/python/mypy_bug/example.py
@@ -0,0 +1,6 @@
+import fake_module
+from my.fake.module import a_tuple
+
+fake_module.frobulate()
+
+foo, bar = a_tuple()
